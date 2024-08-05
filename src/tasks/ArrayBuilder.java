8d0e66cb87fe77702619ef7cc0e434a2b4973050
Aksnovich Ivan <aksnovich.ivan@gmail.com>
2024-08-05 22:31:04 +0300

    second commit

--- src/tasks/ArrayBuilder.java
+++ src/tasks/ArrayBuilder.java
@@ -4,12 +4,21 @@ import java.util.Arrays;
 
 public class ArrayBuilder {
     public static int[] createArray(int len, int initialValue) {
-        int[] array = new int[len];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = initialValue;
+        try {
+            if (len < 0) {
+                throw new NegativeArraySizeException("Передайте методу положительное число");
+            }
+            int[] array = new int[len];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = initialValue;
 
+            }
+            return array;
+
+        } catch (NegativeArraySizeException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+            return new int[0];
         }
-        return array;
 
     }
 
--- src/tasks/LinePrinter.java
+++ src/tasks/LinePrinter.java
@@ -1,10 +1,18 @@
 package tasks;
 
 public class LinePrinter {
-    public static void  printStringMultipleTimes(String line,int number ){
-        for(int i = 0;i<number;i++){
-            System.out.println(line);
+    public static void printStringMultipleTimes(String line, int number) {
+        try {
+            if (number < 0) {
+                throw new IllegalArgumentException("Введите положительное число ");
+            }
+            for (int i = 0; i < number; i++) {
+                System.out.println(line);
 
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
         }
+
     }
 }
