10daab06d537bb9d8b5ad19acc29856d30d96cfd
Aksnovich Ivan <aksnovich.ivan@gmail.com>
2024-08-07 21:25:11 +0300

    second commit

--- src/tasks/ValueComparator.java
+++ src/tasks/ValueComparator.java
@@ -2,7 +2,7 @@ package tasks;
 
 public class ValueComparator {
     public static void compareNumbers() {
-        int a =4;
+        int a = 4;
         int b = 3;
         System.out.println(a >= b ? "a >= b" : "a < b");
     }
