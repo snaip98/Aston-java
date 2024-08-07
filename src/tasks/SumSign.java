35e11d7499a704b9dd055fb1f82fca50e2026eae
Aksnovich Ivan <aksnovich.ivan@gmail.com>
2024-08-07 21:19:16 +0300

    second commit

--- src/tasks/SumSign.java
+++
@@ -1,8 +0,0 @@
-package tasks;
-
-public class SumSign {
-    public static void checkSumSign() {
-        int a = 4, b = 7;
-        System.out.println("Сумма " + (a + b >= 0 ? "положительная" : "отрицательная"));
-    }
-}
