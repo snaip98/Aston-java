79d16ce86de7167c0459c590ebf53a4fef9780fc
Aksnovich Ivan <aksnovich.ivan@gmail.com>
2024-08-26 17:36:29 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MyArray myArray = new MyArray();
         String[][] shouldBeMatrix4x4 = {{"1", "2", "2", "1"}, {"1", "2", "2", "1"}, {"1", "2", "2", "1"}, {"1", "2", "2", "1"}};
-        myArray.checkArray(shouldBeMatrix4x4);
+        MyArray.checkArray(shouldBeMatrix4x4);
     }
 }
--- src/MyArray.java
+++ src/MyArray.java
@@ -2,7 +2,7 @@ import exceptions.MyArrayDataException;
 import exceptions.MyArraySizeException;
 
 public class MyArray {
-    void checkArray(String[][] shouldBeMatrix4x4) {
+    public static void checkArray(String[][] shouldBeMatrix4x4) {
         try {
             if (shouldBeMatrix4x4.length != 4 || shouldBeMatrix4x4[0].length != 4) {
                 throw new MyArraySizeException("Размер массива должен быть 4х4");
@@ -13,14 +13,15 @@ public class MyArray {
                 for (int j = 0; j < shouldBeMatrix4x4[i].length; j++) {
 
                     try {
-                    int intValue = Integer.parseInt(shouldBeMatrix4x4[i][j]) ;
-                    memory += intValue;
-                }catch (NumberFormatException e){
-                    throw new MyArrayDataException("В строке №: "+i+" значение №: "+j+" Невозможно преобразавать в int!");}
+                        int intValue = Integer.parseInt(shouldBeMatrix4x4[i][j]);
+                        memory += intValue;
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException("В строке №: " + i + " значение №: " + j + " Невозможно преобразавать в int!");
+                    }
                 }
             }
             System.out.println(memory);
-        }  catch (MyArraySizeException | MyArrayDataException e) {
+        } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println(e.getMessage());
         }
     }
