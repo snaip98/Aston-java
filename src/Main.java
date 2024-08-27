342b0613933ffa4dc9a82fbf2ccca8a6636ee42c
Aksnovich Ivan <aksnovich.ivan@gmail.com>
2024-08-27 12:23:14 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String[][] shouldBeMatrix4x4 = {{"1", "2", "2", "1"}, {"1", "2", "2", "1"}, {"1", "2", "2", "1"}, {"1", "2", "2", "1"}};
+        String[][] shouldBeMatrix4x4 = {{"1", "%", "2", "1"}, {"1", "2", "2", "1"}, {"1", "2", "2", "1"}, {"1", "2", "2", "1"}};
         MyArray.checkArray(shouldBeMatrix4x4);
     }
 }
