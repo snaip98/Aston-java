784316d9caf92029625c908ea43fdaa373ccbcbc
Aksnovich Ivan <aksnovich.ivan@gmail.com>
2024-09-25 20:08:19 +0300

    first commit

--- src/test/java/PostmanCollectionEchoTest.java
+++ src/test/java/PostmanCollectionEchoTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.config.EncoderConfig;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
@@ -40,6 +41,27 @@ public class PostmanCollectionEchoTest {
                         .extract().response();
         Assertions.assertEquals(response.getStatusCode(), 200);
         Assertions.assertEquals("This is expected to be sent back as part of response body.", response.jsonPath().getString("data"));
+    }
+
+    @Test
+    public void postFormDataRequestTest() {
+        Response response =
+                RestAssured.given().config(RestAssured.config()
+                                .encoderConfig(EncoderConfig.encoderConfig()
+                                        .encodeContentTypeAs("x-www-form-urlencoded",
+                                                ContentType.URLENC)))
+                        .contentType(ContentType.URLENC.withCharset("UTF-8"))
+                        .log().all()
+                        .formParam("foo1", "bar1")
+                        .formParam("foo2", "bar2")
+                        .when()
+                        .post("/post")
+                        .then().log().all()
+                        .extract().response();
+        Assertions.assertEquals(200,response.getStatusCode());
+        Assertions.assertEquals("bar2",response.jsonPath().getString("form.foo2"));
+        Assertions.assertEquals("bar1",response.jsonPath().getString("form.foo1"));
+
     }
 
     @Test
